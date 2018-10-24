aa1c1d16f50ba2960d429769ca4593224bb26ec8
Alex Felipe <alexfelipevieira@gmail.com>
2018-10-24 11:53:24 -0200

    vídeo 3.1

---
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTest.java
@@ -0,0 +1,42 @@
+package br.com.alura.leilao.ui.activity;
+
+import org.junit.Test;
+
+public class LancesLeilaoTelaTest {
+
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberUmLance(){
+//        Salvar leilão na API
+//
+//        Inicializar a main Activity
+//
+//        Clica no leilão
+//
+//        Clica no fab da tela de lances do leilão
+//
+//        Verifica se aparece dialog de aviso por não ter usuáro com títilo e mensagem esperada
+//
+//        Clica no botão "Cadastrar Usuário"
+//
+//        Clica no fab tela de lista de usuários
+//
+//        Clica no EditText e preenche com o nome do usuário
+//
+//        Clica em Adicionar
+//
+//        Clica no back do Android
+//
+//        Clica no fab lances do leilão
+//
+//        Verifica visibilidade do dialog com o título esperado
+//
+//        Clica no edittext de valor e preenche
+//
+//        Seleciona o usuário
+//
+//        Clica no botão "Propor"
+//
+//        Fazer assertion para as views de maior e menor lance, e também, para os maiores lances
+    }
+
+}
