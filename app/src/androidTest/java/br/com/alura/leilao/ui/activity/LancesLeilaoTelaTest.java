f75b66cadc4f24026e96568e8a61fc1348df7a54
Alex Felipe <alexfelipevieira@gmail.com>
2018-10-26 09:39:31 -0200

    vídeo 5.1

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/LancesLeilaoTelaTest.java
@@ -121,6 +121,144 @@ public class LancesLeilaoTelaTest extends BaseTesteIntegracao {
                         isDisplayed())));
     }
 
+    @Test
+    public void deve_AtualizarLancesDoLeilao_QuandoReceberTresLances() throws IOException {
+        tentaSalvarLeilaoNaApi(new Leilao("Carro"));
+
+        mainActivity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(actionOnItemAtPosition(0, click()));
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Usuários não encontrados"),
+                withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+        onView(allOf(withText("Não existe usuários cadastrados! Cadastre um usuário para propor o lance."),
+                withId(android.R.id.message)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withText("Cadastrar usuário"), isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("Alex"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
+
+        onView(allOf(withId(R.id.lista_usuario_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.form_usuario_nome_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("Fran"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(android.R.id.button1),
+                withText("Adicionar"),
+                isDisplayed()))
+                .perform(scrollTo(), click());
+
+        pressBack();
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Novo lance"),
+                withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("200"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario), isDisplayed()))
+                .perform(click());
+        onData(is(new Usuario(1, "Alex")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withText("Propor"), isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Novo lance"),
+                withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("300"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario), isDisplayed()))
+                .perform(click());
+        onData(is(new Usuario(2, "Fran")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withText("Propor"), isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withId(R.id.lances_leilao_fab_adiciona),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Novo lance"),
+                withId(R.id.alertTitle)))
+                .check(matches(isDisplayed()));
+
+        onView(allOf(withId(R.id.form_lance_valor_edittext),
+                isDisplayed()))
+                .perform(click(),
+                        typeText("400"),
+                        closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.form_lance_usuario), isDisplayed()))
+                .perform(click());
+        onData(is(new Usuario(1, "Alex")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+
+        onView(allOf(withText("Propor"), isDisplayed()))
+                .perform(click());
+
+        FormatadorDeMoeda formatador = new FormatadorDeMoeda();
+        onView(withId(R.id.lances_leilao_maior_lance))
+                .check(matches(allOf(withText(formatador.formata(400)),
+                        isDisplayed())));
+        onView(withId(R.id.lances_leilao_menor_lance))
+                .check(matches(allOf(withText(formatador.formata(200)),
+                        isDisplayed())));
+        onView(withId(R.id.lances_leilao_maiores_lances))
+                .check(matches(allOf(withText("400.0 - (1) Alex\n" +
+                                "300.0 - (2) Fran\n" +
+                                "200.0 - (1) Alex\n"),
+                        isDisplayed())));
+    }
+
     @After
     public void teardown() throws IOException {
         limpaBancoDeDadosDaApi();
